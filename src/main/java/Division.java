8466a15660b5fcb44db4f206a434c5958fb9888f
Puram Sai Varshiitha <varshithapuram@gmail.com>
2024-03-19 15:05:40 +0530

    Division of 2 byte numbers.

---
+++ src/main/java/Division.java
@@ -0,0 +1,10 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Division {
+	public void divide() {
+	
+		byte a = 10, b = 2;
+		
+		System.out.println("Result = " + a / b);
+	}
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+package com.qualizeal.javaapp.mainprogram;
+
+import com.qualizeal.javaapp.mathematics.Division;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Division division = new Division();
+		division.divide();
+	}
+}

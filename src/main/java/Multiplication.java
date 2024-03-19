600e552ef9866e6e07e9b66c40c0b07043a8f6ff
Puram Sai Varshiitha <varshithapuram@gmail.com>
2024-03-19 15:12:49 +0530

    Added Multiplication class

---
+++ src/main/java/Multiplication.java
@@ -0,0 +1,10 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Multiplication {
+	public void multiply() {
+	
+		byte a = 10, b = 2;
+		
+		System.out.println("Result = " + a * b);
+	}
+}
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,15 @@
 package com.qualizeal.javaapp.mainprogram;
 
 import com.qualizeal.javaapp.mathematics.Division;
+import com.qualizeal.javaapp.mathematics.Multiplication;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Division division = new Division();
 		division.divide();
+		
+		Multiplication multiplication = new Multiplication();
+		multiplication.multiply();
 	}
 }
